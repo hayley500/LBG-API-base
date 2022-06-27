5bc17ee5f40a44c95ea5354d5e0761db29146225
Hayley <hayleyattwell@hotmail.co.uk>
2022-06-27 17:01:09 +0000

    added a build script

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ app_name=lbg-api-app
 dockerhub_username=hayja500
 tag=latest
 image_name=${dockerhub_username}/${app_name}:${tag} 
+container_port=5000
 
 docker build -t $image_name .
 
@@ -11,6 +12,6 @@ docker push $image_name
 
 docker stop $app_name
 docker rm $app_name
-docker run -d -p 80:8080 --name $app_name $image_name
+docker run -d -p 80:$container_port -e PORT=$container_port --name $app_name $image_name
 
 
